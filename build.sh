25abbc624e7cb4c36b97f8e581fdc5327522afdc
dilluti0n <hskimse1@gmail.com>
2024-11-03 20:40:14 +0900

    Written build script
    build iso with grub and boot it by qemu

---
+++ build.sh
@@ -0,0 +1,19 @@
+set -x
+
+export LIBRARY_PATH="/usr/i686-elf/lib/"
+ASM=i686-elf-as
+CC=i686-elf-gcc
+BINDIR="bin/"
+ISODIR="iso/"
+KERNEL="${BINDIR}/mini.bin"
+ISOKERNEL="${ISODIR}/root/boot/mini.bin"
+ISO="${ISODIR}/mini.iso"
+
+${ASM} start.s -o "${BINDIR}/start.o" &&
+    ${CC} -std=gnu99 -ffreestanding -g -c kernel.c -o "${BINDIR}/kernel.o" &&
+    ${CC} -std=gnu99 -ffreestanding -nostdlib -g -T linker.ld start.o kernel.o -o ${KERNEL} -lgcc &&
+    cp ${KERNEL} ${ISOKERNEL}
+
+if grub-file --is-x86-multiboot ${ISOKERNEL}; then
+   grub-mkrescue iso/root -o ${ISO} && exec qemu-system-i386 ${ISO}
+fi
