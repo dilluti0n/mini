a4f552b5644f228385a8d65499163137328e5e7a
dilluti0n <hskimse1@gmail.com>
2024-11-03 22:01:43 +0900

    Refactor vga_buffer to use 2-dim array
    Rename vga_buffer to term
    Implement escape character \t handling

--- kernel.c
+++ kernel.c
@@ -1,46 +1,51 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#if defined(__linux__)
-#error "This code should not be compiled with a OS-specific compiler"
-#elif !defined(__i386__)
-#error "This code should be compiled with an x86-elf compiler"
-#endif
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
 
-volatile uint16_t *vga_buffer = (uint16_t *)0xB8000;
-
-#define VGA_COLS 80
-#define VGA_ROWS 2
+#define TABWIDTH 8
 
 #define VGA_BUFFER_CHAR(color, char) (((uint16_t)(color) << 8) | (char))
-#define VGA_BUFFER_INDEX(column, row) (VGA_COLS * (column) + (row))
 
-static int term_col = 0;
-static int term_row = 0;
+static uint16_t (*term)[VGA_WIDTH] = (uint16_t (*)[VGA_WIDTH])0xB8000;
+
+static char term_x = 0;		/* 0 to VGA_WIDTH - 1 */
+static char term_y = 0;		/* 0 to VGA_HEIGHT - 1 */
 static uint8_t term_color = 0x0F;	/* Black background, White foreground */
 
+#define TERM_NEWLINE {\
+        term_x = 0; \
+        term_y++;   \
+}
+
+/* Draw the empty screen */
 void term_init()
 {
-	for (int i = 0; i < VGA_BUFFER_INDEX(VGA_COLS, VGA_ROWS); i++)
-		vga_buffer[i] = VGA_BUFFER_CHAR(term_color, ' ');
+	const int max = VGA_WIDTH * VGA_HEIGHT;
+
+	for (int i = 0; i < max; i++)
+		((uint16_t *) term)[i] = VGA_BUFFER_CHAR(term_color, ' ');
 }
 
 void term_putc(char c)
 {
 	switch (c) {
 	case '\n':
-		term_col = 0;
-		term_row++;
+		term_x = 0;
+		term_y++;
+		break;
+	case '\t':
+		term_x += TABWIDTH;
 		break;
 	default:
-		vga_buffer[VGA_BUFFER_INDEX(term_col, term_row++)] =
-			VGA_BUFFER_CHAR(term_color, c);
+		term[term_y][term_x++] = VGA_BUFFER_CHAR(term_color, c);
 		break;
 	}
 
-	if (term_col >= VGA_COLS) {
-		term_col = 0;
-		term_row++;
+	if (term_x >= VGA_WIDTH) {
+		term_x = 0;
+		term_y++;
 	}
 }
 
@@ -50,8 +55,24 @@ void term_print(const char *str, size_t lim)
 		term_putc(str[i]);
 }
 
+size_t strlen(const char *str)
+{
+	size_t i;
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return i;
+}
+
+void term_println(const char *str)
+{
+	term_print(str, strlen(str));
+	term_putc('\n');
+}
+
 void kernel_main()
 {
 	term_init();
-	term_print("Hello, World!\n",sizeof("Hello, World!\n"));
+	term_println("Hello, world!");
+	term_println("This is mini with no version...");
+	term_println("\tWhat should I do next so?");
 }
